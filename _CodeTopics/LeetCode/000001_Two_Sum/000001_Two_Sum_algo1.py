2fb45294727e0d1b9b4835d4af5f2acf740aefed
BIAOXYZ <biaoxyzll@gmail.com>
2019-12-15 23:01:30 +0800

    small local update

--- _CodeTopics/LeetCode/000001_Two_Sum/000001_Two_Sum_algo1.py
+++ _CodeTopics/LeetCode/000001_Two_Sum/000001_Two_Sum_algo1.py
@@ -25,6 +25,8 @@ class Solution(object):
         :rtype: List[int]
         """
 
+        # 等价于
+        ## for i in range(0, len(nums), 1):
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
