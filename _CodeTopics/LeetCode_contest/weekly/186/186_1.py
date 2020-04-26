55bde932cfe9c8c5122c76b5ba0291a8588aaf9e
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2020-04-26 12:09:07 +0800

    Create 186_1.py

---
+++ _CodeTopics/LeetCode_contest/weekly/186/186_1.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def maxScore(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+        def countNumber(s,c):
+            count = 0
+            for i in range(len(s)):
+                if s[i] == c:
+                    count += 1      
+            return count
+        
+        maxpoint = currpoint = 0
+        #for i in range(len(s)):
+        #for i in range(1,len(s)-1):
+        """
+        上面两处浪费了不少时间，第一个注释掉的for是因为至少得给左边分一个字符。第二个注释掉的for错的比较意外：
+        末尾那里是不需要再减1的，否则最后剩两位的时候就不走了。"01"就会直接返回0。
+        """
+        for i in range(1,len(s)):
+            currpoint = countNumber(s[:i],'0') + countNumber(s[i:],'1')
+            if currpoint > maxpoint:
+                maxpoint = currpoint
+        return maxpoint
+    
