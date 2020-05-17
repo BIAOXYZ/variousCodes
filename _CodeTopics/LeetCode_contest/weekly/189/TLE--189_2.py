cb5b88f43ef5ec77393e2a90afea9501788c185b
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2020-05-17 12:10:11 +0800

    Create TLE--189_2.py

---
+++ _CodeTopics/LeetCode_contest/weekly/189/TLE--189_2.py
@@ -0,0 +1,53 @@
+class Solution(object):
+    def arrangeWords(self, text):
+        """
+        :type text: str
+        :rtype: str
+        """
+        
+        text = text[0].lower() + text[1:]
+        length = len(text)
+        
+        dic = {}
+        i = 0
+        while i < length:
+            currword = ''
+            for j in range(i+1,length):
+                # 必须有这个分支处理最后一个单词，否则会陷入死循环。
+                if j == length - 1:
+                    currword = text[i:j+1]
+                    currwordlen = len(currword)
+                    if dic.has_key(currwordlen):
+                        dic[currwordlen].append(currword)
+                    else:
+                        dic[currwordlen] = [currword]
+                    # 这一个赋值也不能少，不然break出去还是继续while循环了。
+                    i = length
+                    break
+                # if text[j] != '': 
+                # 最开始是上面那句，其实是傻了，空字符和只有一个空格的字符没区分。。。
+                if text[j] != ' ':
+                    continue
+                else:
+                    currword = text[i:j]
+                    currwordlen = len(currword)
+                    if dic.has_key(currwordlen):
+                        dic[currwordlen].append(currword)
+                    else:
+                        dic[currwordlen] = [currword]
+                    i = j + 1
+                    break
+        
+        res = ''
+        for i in sorted(dic.keys()):
+            for elem in dic[i]:
+                res = res + ' ' + elem
+        # 因为首单词前也会有个空格，所以要去掉一下。
+        res = res[1:]
+        return res.capitalize()
+    
+"""
+3 / 74 个通过测试用例
+	状态：超出时间限制
+最后执行的输入： "You and i" 
+"""
