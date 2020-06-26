9c505dd0c74d2e3f2a132be1ceeaf41b356e4b4f
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2020-06-26 23:09:54 +0800

    Create WA2--16.18.py

---
+++ _CodeTopics/LeetCode_other_problems/面试题/程序员面试金典(第6版)/16.18/WA2--16.18.py
@@ -0,0 +1,68 @@
+class Solution(object):
+    def patternMatching(self, pattern, value):
+        """
+        :type pattern: str
+        :type value: str
+        :rtype: bool
+        """
+
+        if value == '' and pattern != '':
+            return False
+
+        length = len(value)
+        numa = pattern.count('a')
+        numb = pattern.count('b')
+
+        def check(lena, lenb):
+            worda, wordb = [], []
+            currind = 0
+            for letter in pattern:
+                if letter == 'a':
+                    if value[currind:currind+lena] not in worda:
+                        worda.append(value[currind:currind+lena])
+                    currind += lena
+                    if len(worda) >= 2:
+                        return False
+                else:
+                    if value[currind:currind+lenb] not in wordb:
+                        wordb.append(value[currind:currind+lenb])
+                    currind += lenb
+                    if len(wordb) >= 2:
+                        return False
+            return True
+        
+        if numa != 0 and numb != 0:
+            for lena in range(length/numa + 1):
+                for lenb in range((length-numa*lena)/numb + 1):
+                    if numa*lena + numb*lenb == length:
+                        if check(lena, lenb):
+                            return True
+        elif numa != 0 and numb == 0:
+            for lena in range(length/numa + 1):
+                if numa*lena == length:
+                    if check(lena, 0):
+                        return True
+        elif numa == 0 and numb != 0:
+            for lenb in range(length/numb + 1):
+                if numb*lenb == length:
+                    if check(0, lenb):
+                        return True
+        else:
+            if value == '':
+                return True
+        return False
+
+"""
+https://leetcode-cn.com/submissions/detail/82252675/
+
+91 / 93 个通过测试用例
+状态：解答错误
+
+输入："a"
+      ""
+输出：false
+预期：true
+"""
+"""
+# 加了第9，10行代码，结果比上一个还少过了一个用例。。。这个题的这种边界的值没啥意思，核心算法写出来就行。
+"""
