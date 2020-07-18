60d3b950be8ab926a23cc64451206db031607cf8
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2020-07-18 23:30:28 +0800

    Create RE2--000097.py

---
+++ _CodeTopics/LeetCode/000097-h/RE2--000097.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    def isInterleave(self, s1, s2, s3):
+        """
+        :type s1: str
+        :type s2: str
+        :type s3: str
+        :rtype: bool
+        """
+
+        len1, len2, len3 = len(s1), len(s2), len(s3)
+        if len3 != len1 + len2:
+            return False
+        if len1 == 0 and len2 == 0:
+            return True
+        elif len1 == 0:
+            if s2 == s3: return True
+            else: return False
+        elif len2 == 0:
+            if s1 == s3: return True
+            else: return False
+
+        dp = [[False for i in range(len1 + 1)] for j in range(len2 + 1)]
+        dp[0][0] = True
+
+        for i in range(0, len1 + 1):
+            for j in range(0, len2 + 1):
+                pos = i + j - 1
+                if i > 0:
+                    dp[i][j] = dp[i][j] or (s3[pos] == s1[i-1] and dp[i-1][j])
+                if j > 0:
+                    dp[i][j] = dp[i][j] or (s3[pos] == s2[j-1] and dp[i][j-1])
+        return dp[len1][len2]
+        
+"""
+14 / 101 个通过测试用例
+状态：执行出错
+
+执行出错信息：Line 29: IndexError: list index out of range
+最后执行的输入："db"
+               "b"
+               "cbb"
+"""
