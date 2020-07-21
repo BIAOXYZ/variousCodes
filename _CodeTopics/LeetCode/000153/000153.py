69966c3e1c733497137d8cb34ba97f581d9f863e
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2020-07-22 02:23:48 +0800

    Create 000153.py

---
+++ _CodeTopics/LeetCode/000153/000153.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        # 手动狗头。。。
+        return min(nums)
+        
+"""
+https://leetcode-cn.com/submissions/detail/90188056/
+
+146 / 146 个通过测试用例
+状态：通过
+执行用时：12 ms
+内存消耗：13 MB
+"""
+"""
+执行用时：12 ms, 在所有 Python 提交中击败了98.45%的用户
+内存消耗：13 MB, 在所有 Python 提交中击败了33.33%的用户
+"""
