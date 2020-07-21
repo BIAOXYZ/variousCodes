6106808a84d7a551398a9d967302d6b14fcc68e1
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2020-07-22 02:20:51 +0800

    Create 000154.py

---
+++ _CodeTopics/LeetCode/000154-h/000154.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        # 手动狗头。。。
+        return min(nums)
+        
+"""
+https://leetcode-cn.com/submissions/detail/90188021/
+
+192 / 192 个通过测试用例
+状态：通过
+执行用时：20 ms
+内存消耗：12.9 MB
+"""
+"""
+执行用时：20 ms, 在所有 Python 提交中击败了77.74%的用户
+内存消耗：12.9 MB, 在所有 Python 提交中击败了100.00%的用户
+"""
