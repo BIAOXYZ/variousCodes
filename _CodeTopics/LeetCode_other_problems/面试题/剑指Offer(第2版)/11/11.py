9050589646dc2128ff0136d411e1293bfc4dc138
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2020-07-22 02:05:23 +0800

    Create 11.py

---
+++ _CodeTopics/LeetCode_other_problems/面试题/剑指Offer(第2版)/11/11.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def minArray(self, numbers):
+        """
+        :type numbers: List[int]
+        :rtype: int
+        """
+        
+        # 手动狗头。。。
+        return min(numbers)
+        
+"""
+https://leetcode-cn.com/submissions/detail/90187530/
+
+192 / 192 个通过测试用例
+状态：通过
+执行用时：24 ms
+内存消耗：13 MB
+"""
+"""
+执行用时：24 ms, 在所有 Python 提交中击败了51.62%的用户
+内存消耗：13 MB, 在所有 Python 提交中击败了100.00%的用户
+"""
