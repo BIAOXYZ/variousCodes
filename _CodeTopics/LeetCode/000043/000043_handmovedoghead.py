abda293a3ac882945913aab9a710445bdede5624
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2020-08-13 16:56:37 +0800

    Create 000043_handmovedoghead.py

---
+++ _CodeTopics/LeetCode/000043/000043_handmovedoghead.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+
+        # 手动狗头题
+        return str(int(num1) * int(num2))
+        
+"""
+https://leetcode-cn.com/submissions/detail/97700575/
+
+311 / 311 个通过测试用例
+状态：通过
+执行用时：24 ms
+内存消耗：12.8 MB
+"""
+"""
+执行用时：24 ms, 在所有 Python 提交中击败了89.93%的用户
+内存消耗：12.8 MB, 在所有 Python 提交中击败了15.62%的用户
+"""
