6fd71dc54df27b6ac1ca649e266712d4b627498b
BIAOXYZ <liulliu@cn.ibm.com>
2020-09-12 18:53:06 +0800

    Create 1.py

---
+++ _CodeTopics/LeetCode_contest/season/2020-fall/1.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+        res = [1, 0]   
+        for letter in s:
+            if letter == 'A':
+                res[0] = 2 * res[0] + res[1]
+            else:
+                res[1] = 2 * res[1] + res[0]
+        return res[0] + res[1]
+    
