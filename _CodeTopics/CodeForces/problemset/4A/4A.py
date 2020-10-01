789f342997e39fa56a511bb041a0ac03fdedf5ff
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2020-10-01 11:50:39 +0800

    Create 4A.py

---
+++ _CodeTopics/CodeForces/problemset/4A/4A.py
@@ -0,0 +1,12 @@
+weight = raw_input()
+remainder = int(weight) % 2
+if int(weight) < 3:
+    print "NO"
+elif remainder:
+    print "NO"
+else:
+    print "YES" 
+
+"""
+https://codeforces.com/problemset/submission/4/94371519
+"""
