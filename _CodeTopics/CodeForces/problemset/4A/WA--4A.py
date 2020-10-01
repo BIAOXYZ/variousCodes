0a7bd0a3340294426ea10c1ee5572622461d0e85
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2020-10-01 11:41:53 +0800

    Create WA--4A.py

---
+++ _CodeTopics/CodeForces/problemset/4A/WA--4A.py
@@ -0,0 +1,21 @@
+weight = raw_input()
+remainder = int(weight) % 2
+if remainder:
+    print "NO"
+else:
+    print "YES" 
+
+"""
+https://codeforces.com/problemset/submission/4/94371036
+
+Time: 278 ms, memory: 4 KB
+Verdict: WRONG_ANSWER
+Input
+2
+Participant's output
+YES
+Jury's answer
+NO
+Checker comment
+wrong answer expected NO, found YES
+"""
