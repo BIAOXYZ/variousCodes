75409b028c48d62094bf64fbc582ed7ca0002dca
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2020-10-07 01:13:00 +0800

    Create 000075_handmovedoghead.py

---
+++ _CodeTopics/LeetCode/000075/000075_handmovedoghead.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+
+        # 手动狗头题
+        nums.sort()
+        
+"""
+https://leetcode-cn.com/submissions/detail/113730420/
+
+87 / 87 个通过测试用例
+状态：通过
+执行用时: 20 ms
+内存消耗: 12.6 MB
+
+执行用时：20 ms, 在所有 Python 提交中击败了65.88%的用户
+内存消耗：12.6 MB, 在所有 Python 提交中击败了5.14%的用户
+"""
