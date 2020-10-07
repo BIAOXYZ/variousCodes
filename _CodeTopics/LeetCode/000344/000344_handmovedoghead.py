754ba462842afefb445ed3c1e6d442384245b1ce
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2020-10-08 01:35:41 +0800

    Create 000344_handmovedoghead.py

---
+++ _CodeTopics/LeetCode/000344/000344_handmovedoghead.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+
+        # 手动狗头题
+        s.reverse()
+        
+"""
+https://leetcode-cn.com/submissions/detail/113949096/
+
+478 / 478 个通过测试用例
+状态：通过
+执行用时: 40 ms
+内存消耗: 18.7 MB
+
+执行用时：40 ms, 在所有 Python 提交中击败了91.03%的用户
+内存消耗：18.7 MB, 在所有 Python 提交中击败了80.08%的用户
+"""
