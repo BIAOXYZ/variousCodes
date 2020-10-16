cecf529891b33cb8dad48a39b638c07ed26b13a3
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2020-10-16 15:39:33 +0800

    Create 000977_algo2.py

---
+++ _CodeTopics/LeetCode/000977/000977_algo2.py
@@ -0,0 +1,56 @@
+class Solution(object):
+    def sortedSquares(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        """
+
+        length = len(A)
+
+        if A[0] >= 0:
+            f = lambda x: x ** 2
+            return [f(elem) for elem in A]
+            # 上面两句可以用下面一句替代。
+            # return [(lambda x: x ** 2)(elem) for elem in A]       
+        if A[length-1] < 0:
+            # 这里当然可以继续先定义lambda函数，然后代入：
+            #     g = lambda x: x ** 2
+            #     return [f(elem) for elem in A]
+            # 不过下面这种写法才体现了lambda匿名函数的精髓吧。
+            return reversed([(lambda x: x ** 2)(elem) for elem in A])
+
+        # 官方答案里有两种双指针，第一种是“方法三 双指针”，也就是传统的从两边向中间。
+        # 这是第二种，两个指针从中间向两边移动。
+        left = 0
+        while A[left] < 0:
+            left += 1
+        right, left = left, left - 1
+
+        res = []
+        while left >= 0 and right <= length-1:
+            if abs(A[left]) <= A[right]:
+                res.append(A[left]**2)
+                left -= 1
+            else:
+                res.append(A[right]**2)
+                right += 1
+        # 一个指针到头后，把剩下的元素依次平方后贴到结果数组末尾。
+        if left == -1:
+            for i in range(right, length):
+                res.append(A[i]**2)
+        elif right == length:
+            for i in range(left, -1, -1):
+                res.append(A[i]**2)
+        return res
+        
+"""
+https://leetcode-cn.com/submissions/detail/116233930/
+
+132 / 132 个通过测试用例
+状态：通过
+执行用时: 192 ms
+内存消耗: 15 MB
+
+执行用时：192 ms, 在所有 Python 提交中击败了94.95%的用户
+内存消耗：15 MB, 在所有 Python 提交中击败了6.03%的用户
+"""
