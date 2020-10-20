5460baaaf515bd34b14c5435cc90ef0797399d36
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2020-10-20 11:24:56 +0800

    Create 000143_algo2.py

---
+++ _CodeTopics/LeetCode/1-200/000143/000143_algo2.py
@@ -0,0 +1,46 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reorderList(self, head):
+        """
+        :type head: ListNode
+        :rtype: None Do not return anything, modify head in-place instead.
+        """
+
+        # 上一个原地的算法竟然超时了。。。所以其实就是得搞有额外空间但是快的呗。。。来了~
+        # 先用最基本的list（就不信还会超时），然后再写个deque的。
+
+        nodeArr, curr = [], head
+        while curr:
+            nodeArr.append(curr)
+            curr = curr.next
+
+        if len(nodeArr) <= 2:
+            return head
+        
+        while nodeArr:
+            currHead = nodeArr.pop(0)
+            if nodeArr:
+                currTail = nodeArr.pop()
+            else:
+                # 断开最后的单个节点的链接，否则会成环。
+                currHead.next = None
+                break
+            currHead.next = currTail
+            currTail.next = nodeArr[0] if nodeArr else None
+        return head
+        
+"""
+https://leetcode-cn.com/submissions/detail/117148352/
+
+13 / 13 个通过测试用例
+状态：通过
+执行用时: 124 ms
+内存消耗: 32.7 MB
+
+执行用时：124 ms, 在所有 Python 提交中击败了7.00%的用户
+内存消耗：32.7 MB, 在所有 Python 提交中击败了6.62%的用户
+"""
