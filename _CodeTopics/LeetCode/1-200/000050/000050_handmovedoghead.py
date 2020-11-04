7a4ea54d0de969baf0fe3acafc037d81765105ae
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2020-11-05 01:12:44 +0800

    Create 000050_handmovedoghead.py

---
+++ _CodeTopics/LeetCode/1-200/000050/000050_handmovedoghead.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+
+        # 手动狗头题
+        return x**n*1.0
+        
+"""
+https://leetcode-cn.com/submissions/detail/121112261/
+
+304 / 304 个通过测试用例
+状态：通过
+执行用时: 16 ms
+内存消耗: 13.1 MB
+
+执行用时：16 ms, 在所有 Python 提交中击败了89.75%的用户
+内存消耗：13.1 MB, 在所有 Python 提交中击败了5.01%的用户
+"""
