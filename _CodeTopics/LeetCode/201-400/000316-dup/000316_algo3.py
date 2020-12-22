f431fbd3751b73335d652c6ac47eb800d832dd46
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2020-12-22 17:01:40 +0800

    Create 000316_algo3.py

---
+++ _CodeTopics/LeetCode/201-400/000316-dup/000316_algo3.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    def removeDuplicateLetters(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+        def str_to_dict(s):
+            dic = {}
+            for ch in s:
+                if dic.has_key(ch):
+                    dic[ch] += 1
+                else:
+                    dic[ch] = 1
+            return dic
+
+        length = len(s)
+        dic = str_to_dict(s)
+        monotoneStk = []
+
+        for i in range(length):
+            if s[i] not in monotoneStk:
+                while monotoneStk and monotoneStk[-1] > s[i]:
+                    if dic[monotoneStk[-1]] > 0:
+                        monotoneStk.pop()
+                    else:
+                        break
+                monotoneStk.append(s[i])
+            dic[s[i]] -= 1
+        return ''.join(monotoneStk)
+        
+"""
+https://leetcode-cn.com/submissions/detail/132902778/
+
+289 / 289 个通过测试用例
+状态：通过
+执行用时: 48 ms
+内存消耗: 13.5 MB
+
+执行用时：48 ms, 在所有 Python 提交中击败了11.06%的用户
+内存消耗：13.5 MB, 在所有 Python 提交中击败了6.25%的用户
+"""
