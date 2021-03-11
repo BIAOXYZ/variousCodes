9bf82480ba95b79962c522bf10ae649ab2cfa051
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2021-03-12 02:55:19 +0800

    Create WA3--000331.py

---
+++ _CodeTopics/LeetCode/201-400/000331/WA3--000331.py
@@ -0,0 +1,50 @@
+class Solution(object):
+    def isValidSerialization(self, preorder):
+        """
+        :type preorder: str
+        :rtype: bool
+        """
+
+        length = len(preorder)
+        if length == 0 or \
+            (length == 1 and preorder[0] != "#") or \
+            (length > 1 and preorder[0] == "#"):
+            return False
+
+        # direction通过长度和值，来表示当前应该去搜索第 len(direction) 层的左/右子树了。
+        # 比如初始时 ['l'] 表示当前应该去遍历第一层（也就是根结点）的左子树了。
+        direction = ['l']
+
+        for i in range(1, length):
+            ch = preorder[i]
+            if ch == ",":
+                continue
+            if ch.isdigit():
+                direction.append("l")
+            elif ch == "#":
+                if direction[-1] == "l":
+                    direction[-1] = "r"
+                else:
+                    while direction and direction[-1] == "r":
+                        direction.pop()
+                    if direction:
+                        direction[-1] = "r"
+            ##print "ch is:", ch
+            ##print direction
+            if (not direction and i < length - 1) or (direction and i == length - 1):
+                return False
+        return True
+        
+"""
+https://leetcode-cn.com/submissions/detail/154098673/
+
+142 / 150 个通过测试用例
+状态：解答错误
+
+输入：
+"9,#,92,#,#"
+输出：
+false
+预期：
+true
+"""
