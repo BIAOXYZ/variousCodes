4065f8666f81a4a882d458d45fe4f79c8af1d4de
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2021-03-15 01:03:58 +0800

    Create 000054_optm.py

---
+++ _CodeTopics/LeetCode/1-200/000054/000054_optm.py
@@ -0,0 +1,63 @@
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+
+        # 又优化了一点点的版本。
+
+        def getOuterElem(mtx, res):
+            h = len(mtx)
+            w = len(mtx[0])
+            if h == 1:
+                for i in range(w):
+                    res.append(mtx[0][i])
+                return res
+            if w == 1:
+                for i in range(h):
+                    res.append(mtx[i][0])
+                return res
+            for i in range(w):
+                res.append(mtx[0][i])
+            for i in range(1,h):
+                res.append(mtx[i][w-1])
+            for i in range(w-2,-1,-1):
+                res.append(mtx[h-1][i])
+            for i in range(h-2,0,-1):
+                res.append(mtx[i][0])
+        
+        def getInnerMatrix(mtx):
+            mtx.remove(mtx[0])
+            mtx.remove(mtx[-1])
+            for row in mtx:
+                row.remove(row[0])
+                row.pop()
+
+        if matrix == []:
+            return []
+        res = []
+        h = len(matrix)
+        w = len(matrix[0])
+        while h > 1 and w > 1:
+            getOuterElem(matrix, res)
+            getInnerMatrix(matrix)
+            h -= 2
+            w -= 2
+        if h == 0 or w == 0:
+            return res
+        else:
+            getOuterElem(matrix, res)
+            return res
+        
+"""
+https://leetcode-cn.com/submissions/detail/155234577/
+
+22 / 22 个通过测试用例
+状态：通过
+执行用时: 20 ms
+内存消耗: 12.9 MB
+
+执行用时：20 ms, 在所有 Python 提交中击败了45.20%的用户
+内存消耗：12.9 MB, 在所有 Python 提交中击败了87.78%的用户
+"""
