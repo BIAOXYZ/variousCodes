03e6ac4374d3e4aaf12b0755dff2eaf8128f9757
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2021-04-07 00:24:34 +0800

    Create 000081_handmovedoghead.py

---
+++ _CodeTopics/LeetCode/1-200/000081/000081_handmovedoghead.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+
+        # 这个手动狗头也太狗了。。。
+        return target in nums
+        
+"""
+https://leetcode-cn.com/submissions/detail/164581331/
+
+279 / 279 个通过测试用例
+状态：通过
+执行用时: 20 ms
+内存消耗: 13.2 MB
+
+执行用时：20 ms, 在所有 Python 提交中击败了78.43%的用户
+内存消耗：13.2 MB, 在所有 Python 提交中击败了79.90%的用户
+"""
