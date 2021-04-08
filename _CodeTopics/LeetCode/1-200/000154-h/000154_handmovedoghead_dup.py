69537d91a27fb7c1342a7b340e8be3f01af4b3ec
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2021-04-09 00:54:47 +0800

    Create 000154_handmovedoghead_dup.py

---
+++ _CodeTopics/LeetCode/1-200/000154-h/000154_handmovedoghead_dup.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        # hard又如何？还是手动狗头伺候。。。
+        return min(nums)
+        
+"""
+https://leetcode-cn.com/submissions/detail/165519602/
+
+192 / 192 个通过测试用例
+状态：通过
+执行用时: 24 ms
+内存消耗: 13.4 MB
+
+执行用时：24 ms, 在所有 Python 提交中击败了43.50%的用户
+内存消耗：13.4 MB, 在所有 Python 提交中击败了35.03%的用户
+"""
