c88fa800001a005e37156093f2d179a35e22f5ea
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2021-04-20 01:58:03 +0800

    Create 000028_handmovedoghead.py

---
+++ _CodeTopics/LeetCode/1-200/000028/000028_handmovedoghead.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+
+        # 手动狗头题
+        return haystack.find(needle)
+        
+"""
+https://leetcode-cn.com/submissions/detail/169826549/
+
+79 / 79 个通过测试用例
+状态：通过
+执行用时: 20 ms
+内存消耗: 13.3 MB
+
+执行用时：20 ms, 在所有 Python 提交中击败了74.60%的用户
+内存消耗：13.3 MB, 在所有 Python 提交中击败了35.44%的用户
+"""
