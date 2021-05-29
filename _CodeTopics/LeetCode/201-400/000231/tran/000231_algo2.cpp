134677d613a0311472454648102803f755f51d4b
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2021-05-30 02:44:54 +0800

    Create 000231_algo2.cpp

---
+++ _CodeTopics/LeetCode/201-400/000231/tran/000231_algo2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n <= 0)
+            return false;
+        if ((n & n - 1) != 0) {
+            return false;
+        }
+        return true;
+    }
+};
+
+/*
+https://leetcode-cn.com/submissions/detail/182053268/
+
+1108 / 1108 个通过测试用例
+状态：通过
+执行用时: 8 ms
+内存消耗: 5.8 MB
+
+执行用时：8 ms, 在所有 C++ 提交中击败了43.12%的用户
+内存消耗：5.8 MB, 在所有 C++ 提交中击败了25.72%的用户
+*/
