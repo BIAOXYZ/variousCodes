699c69683ef0cf564b585fc48ed8cc065a698373
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2021-05-30 12:20:41 +0800

    Create WA--243_2.py

---
+++ _CodeTopics/LeetCode_contest/weekly/weekly2021/243/WA--243_2.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def maxValue(self, n, x):
+        """
+        :type n: str
+        :type x: int
+        :rtype: str
+        """
+        
+        length = len(n)
+        res = ''
+        if n[0] != '-':
+            for i in range(length):
+                ch = n[i]
+                if int(ch) > x:
+                    continue
+                else:
+                    res = n[:i] + str(x) + n[i:]
+                    return res
+            res = n + str(x)
+            return res
+        else:
+            for i in range(1, length):
+                ch = n[i]
+                if int(ch) < x:
+                    continue
+                else:
+                    res = n[:i] + str(x) + n[i:]
+                    return res
+            res = n + str(x)
+            return res
+    
+"""
+https://leetcode-cn.com/submissions/detail/182109553/
+
+81 / 97 个通过测试用例
+状态：解答错误
+
+最后执行的输入：
+"-132"
+3
+"""
