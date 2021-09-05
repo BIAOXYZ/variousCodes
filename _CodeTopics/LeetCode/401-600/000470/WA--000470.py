24dadf3d6f2aa00cae005c18d121da9c8ff0ec3e
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2021-09-05 22:36:07 +0800

    Create WA--000470.py

---
+++ _CodeTopics/LeetCode/401-600/000470/WA--000470.py
@@ -0,0 +1,53 @@
+# The rand7() API is already defined for you.
+# def rand7():
+# @return a random integer in the range 1 to 7
+
+class Solution(object):
+    def rand10(self):
+        """
+        :rtype: int
+        """
+
+        """
+        # 比如我们可以利用两个 Rand7() 相乘，分别可以得到结果如下：
+
+            1	2	3	4	5	6	7
+        1	1	2	3	4	5	6	7
+        2	2	4	6	8	10	12	14
+        3	3	6	9	12	15	18	21
+        4	4	8	12	16	20	24	28
+        5	5	10	15	20	25	30	35
+        6	6	12	18	24	30	36	42
+        7	7	14	21	28	35	42	49
+        """
+        """
+        # 我们可以得到每个数生成的概率为:
+
+            1	    2	    3	    4	    5	    6	    7	    8	    9
+        P	1/49	2/49	2/49	3/49	2/49	4/49	2/49	2/49	4/49
+            10	    12	    14	    15	    16	    18	    20	    21	    24
+        P	2/49	1/49	2/49	2/49	2/49	1/49	2/49	2/49	2/49
+            25	    28	    30	    35	    36	    42	    49		
+        P	1/49	2/49	2/49	2/49	1/49	2/49	1/49	
+        """
+
+        se = set([2,3,5,7,8,10,14,15,16,20])
+        dic = {
+            2:1, 3:2, 5:3, 7:4, 8:5, 10:6, 14:7, 15:8, 16:9, 20:10
+        }
+        while True:
+            first, second = rand7(), rand7()
+            if first * second not in se:
+                continue
+            else:
+                return dic[first * second]
+        
+"""
+https://leetcode-cn.com/submissions/detail/215594783/
+
+9 / 12 个通过测试用例
+状态：解答错误
+
+最后执行的输入：
+10000
+"""
