fa0c340a2230a715d7a8f04af2b6c33c3d4b01c0
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2021-10-07 03:02:53 +0800

    Create WA--000434.py

---
+++ _CodeTopics/LeetCode/401-600/000434/WA--000434.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def countSegments(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        # 手动狗头题
+        return len(s.split(' '))
+        
+"""
+https://leetcode-cn.com/submissions/detail/226336878/
+
+16 / 27 个通过测试用例
+状态：解答错误
+
+输入：
+""
+输出：
+1
+预期结果：
+0
+"""
