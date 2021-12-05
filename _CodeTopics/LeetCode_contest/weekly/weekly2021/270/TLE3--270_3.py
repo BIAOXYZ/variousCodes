46f882270c32af3c4568e679123c03df5919de61
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2021-12-05 13:22:08 +0800

    Create TLE3--270_3.py

---
+++ _CodeTopics/LeetCode_contest/weekly/weekly2021/270/TLE3--270_3.py
@@ -0,0 +1,48 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def getDirections(self, root, startValue, destValue):
+        """
+        :type root: Optional[TreeNode]
+        :type startValue: int
+        :type destValue: int
+        :rtype: str
+        """
+        
+        paths = ['','']
+        def dfs_with_path(node, path):
+            if node.val == startValue:
+                paths[0] = path
+            if node.val == destValue:
+                paths[1] = path
+            if paths[0] != '' and paths[1] != '':
+                return
+            if node.left:
+                dfs_with_path(node.left, path + 'L')
+            if node.right:
+                dfs_with_path(node.right, path + 'R')
+        
+        dfs_with_path(root, '')
+        ##print paths
+        
+        commonPrefixLen = 0
+        for i in range(min(len(paths[0]), len(paths[1]))):
+            if paths[0][i] == paths[1][i]:
+                commonPrefixLen += 1
+            else:
+                break
+        pathLeft = paths[0][commonPrefixLen:]
+        pathRight = paths[1][commonPrefixLen:]
+        
+        return 'U'*len(pathLeft) + pathRight
+    
+"""
+https://leetcode-cn.com/submissions/detail/245397812/
+
+287 / 332 个通过测试用例
+状态：超出时间限制
+"""
