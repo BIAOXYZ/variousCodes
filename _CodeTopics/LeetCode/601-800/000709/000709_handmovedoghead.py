8ad1d847af89715a398378fd4f966b1523b0e2df
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2021-12-12 01:08:23 +0800

    Create 000709_handmovedoghead.py

---
+++ _CodeTopics/LeetCode/601-800/000709/000709_handmovedoghead.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def toLowerCase(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+        # 手动狗头题
+        return s.lower()
+        
+"""
+https://leetcode-cn.com/submissions/detail/247601183/
+
+执行用时：20 ms, 在所有 Python 提交中击败了33.98%的用户
+内存消耗：13 MB, 在所有 Python 提交中击败了59.22%的用户
+通过测试用例：
+114 / 114
+"""
