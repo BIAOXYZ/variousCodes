b406ae0a7d4f8acff777dee504a6118a4ee83dab
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2021-12-19 12:41:29 +0800

    Create TLE4--272_2.py

---
+++ _CodeTopics/LeetCode_contest/weekly/weekly2021/272/TLE4--272_2.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def addSpaces(self, s, spaces):
+        """
+        :type s: str
+        :type spaces: List[int]
+        :rtype: str
+        """
+        
+        n = len(spaces)
+        n1 = len(s)
+        spaces = [spaces[i] + i for i in range(n)]
+        se = set(spaces)
+        
+        used = 0
+        res = ''
+        for i in range(n + n1):
+            if i in se:
+                res += ' '
+                used += 1
+            else:
+                res += s[i - used]
+        return res
+    
+"""
+https://leetcode-cn.com/submissions/detail/249863450/
+
+59 / 66 个通过测试用例
+状态：超出时间限制
+"""
