e85187aadfb1699f0ee4db0eae20b63113dd95bd
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2022-02-20 12:52:54 +0800

    Create WA--281_3.py

---
+++ _CodeTopics/LeetCode_contest/weekly/weekly2022/281-[LeetCode周赛宕机]/WA--281_3.py
@@ -0,0 +1,15 @@
+
+"""
+https://leetcode-cn.com/submissions/detail/270626650/
+
+123 / 150 个通过测试用例
+状态：解答错误
+
+输入：
+"xyutfpopdynbadwtvmxiemmusevduloxwvpkjioizvanetecnuqbqqdtrwrkgt"
+1
+输出：
+"zyxyxwxwvwvuvuvututstststrqrqpqpopononmnmlmkjkigigifededededcbaba"
+预期：
+"zyxyxwxwvwvuvuvututstrtrtqpqpqponononmlmkmkjigifiededededcbaba"
+"""
