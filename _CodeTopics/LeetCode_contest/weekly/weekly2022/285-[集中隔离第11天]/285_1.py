a8dc8c911df44848ded3e87e0ee40c201330be9f
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2022-03-20 12:14:59 +0800

    Create 285_1.py

---
+++ _CodeTopics/LeetCode_contest/weekly/weekly2022/285-[集中隔离第11天]/285_1.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def countHillValley(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        q = []
+        for num in nums:
+            if not q or q[-1] != num:
+                q.append(num)
+        
+        n = len(q)
+        if n < 3:
+            return 0
+        res = 0
+        for i in range(1, n-1):
+            if (q[i]-q[i-1]) * (q[i]-q[i+1]) > 0:
+                res += 1
+        return res
+    
+"""
+https://leetcode-cn.com/submissions/detail/286170792/
+
+119 / 119 个通过测试用例
+状态：通过
+执行用时: 20 ms
+内存消耗: 13.1 MB
+"""
