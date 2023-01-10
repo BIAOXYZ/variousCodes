78c635bc4383187be69bbe7f595ec96e1e24efc2
BIAOXYZ <BIAOXYZLL@GMAIL.COM>
2023-01-11 03:35:42 +0800

    Create unfinished--79_1.cpp

---
+++ _CodeTopics/LeetCode/2001-2200/002283--biweekly79_1/unfinished--79_1.cpp
@@ -0,0 +1,43 @@
+
+class Counter {
+public:
+    unordered_map<char, int> _ctr;
+
+    Counter(string s) {
+        for (char ch : s) {
+            if (_ctr.find(ch) != _ctr.end()) {
+                _ctr[ch]++;
+            } else {
+                _ctr[ch] = 1;
+            }
+        }
+    }
+
+    ~Counter();
+};
+
+class Solution {
+public:
+    bool digitCount(string num) {
+
+        // 第 79 场双周赛第一题
+        // https://leetcode.cn/submissions/detail/319178335/
+
+        int n = num.size();
+        Counter myCtr = Counter(num);
+        for (int i = 0; i < n; ++i) {
+            char ch = num[i];
+            char i_char;
+            // 就下面这句，想把 int 1 转成 char '1'，一时查不到，凌晨3点半多了，累死我了。CNMCNMCNM！！！
+            if (myCtr._ctr[std::sprintf(i_char, "%d", i)] != atoi(ch)) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+
+/*
+
+*/
